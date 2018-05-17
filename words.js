700f35e06f8f226786dd6f83eee16df8c265fcd1
Marcelo <marceloxerox@hotmail.com>
2018-05-16 19:08:35 -0700

    Bands List

---
+++ words.js
@@ -0,0 +1,15 @@
+//An array of words that'll be used in hangman.
+exports.newWord = {
+  wordList: [ "megadeth", 
+              "slayer", 
+              "metallica", 
+              "iron maiden", 
+              "judas priest",
+              "slipknot", 
+              "children of bodom", 
+              "tool", 
+              "Korn",
+              "Disturbed",
+              "Sepultura",
+              "lamb of god" ]
+};
